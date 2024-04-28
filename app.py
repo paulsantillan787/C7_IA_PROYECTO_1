0aa1cfd95b9986d49d944ba31c09534dd82f314e
MiguelVaDu <angel301001_vd@unmsm.edu.pe>
2024-04-28 08:35:53 -0500

    Condiciones meta

--- app.py
+++ app.py
@@ -550,7 +550,36 @@ while running:
           dibujarPosition(elemento[0][0], elemento[0][1], WHITE, 2)
         elif(elemento[1] == 'B3'):
           dibujarPosition(elemento[0][0], elemento[0][1], WHITE, 3)
-          
+  
+  
+  if f1n + f2n + f3n == 2:
+    print ('Ganan blancas')
+    running = False
+
+  if f1b + f2b + f3b == 2:
+    print ('Ganan negras')
+    running = False
+
+  if f1b == 0 or f2b == 0 or f3b == 0:
+    print ('Ganan negras')
+    running = False
+
+  if f1n == 0 or f2n == 0 or f3n == 0:
+    print ('Ganan blancas')
+    running = False
+
+  if not fbm and fnm:
+    print ('Ganan negras')
+    running = False
+
+  if not fnm and fbm:
+    print ('Ganan blancas')
+    running = False
+
+  if not fbm and not fnm:
+    print('empate')
+    running = False
+    
   # Update the display
   pygame.display.update()
   
